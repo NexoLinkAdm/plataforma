69ac0e912787027431212c3826691537d712f959
NexoLinkAdm <nexolinkadm@gmail.com>
2025-08-04 10:18:37 +0100

    atualiza├º├úo autom├ítica

--- public/index.php
+++ public/index.php
@@ -5,23 +5,16 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Manutenção
-if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Autoload (vendor)
-require __DIR__.'/vendor/autoload.php';
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap e inicialização da app
+// Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__.'/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-// Handle da requisição
-$response = $app->handle(
-    $request = Request::capture()
-);
-
-$response->send();
-
-$app->terminate($request, $response);
+$app->handleRequest(Request::capture());
