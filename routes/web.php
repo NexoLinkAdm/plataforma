3b6019f908f09cbc2ef02d4d507c085afcd65288
NexoLinkAdm <nexolinkadm@gmail.com>
2025-08-05 10:22:06 +0100

    atualiza├º├úo autom├ítica

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,17 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware(['auth'])->group(function () {
-    // Perfil
+Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-    // Mercado Pago
+// Rotas para o fluxo de conexão do Mercado Pago (protegidas por autenticação)
+Route::middleware(['auth'])->group(function () {
     Route::get('/conectar-mercadopago', [MercadoPagoController::class, 'redirectToOAuth'])->name('mp.connect');
     Route::get('/oauth/callback', [MercadoPagoController::class, 'handleOAuthCallback'])->name('mp.callback');
 });
-
 
 
 require __DIR__.'/auth.php';
