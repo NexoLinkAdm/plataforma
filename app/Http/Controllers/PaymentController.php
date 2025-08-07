74ea53da03d018de1382bf08c48f41a0d23947a6
NexoLinkAdm <nexolinkadm@gmail.com>
2025-08-07 13:23:20 +0100

    atualiza├º├úo autom├ítica

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -18,7 +18,7 @@ use MercadoPago\Exceptions\MPApiException;
 use MercadoPago\MercadoPagoConfig;
 
 
-//versão de teste para faze boloteos e pix funcionar a primeria tentativa
+//versão de teste para faze boloteos e pix funcionar a primeria tentativa 01
 class PaymentController extends Controller
 {
     public function processPayment(Request $request): JsonResponse
